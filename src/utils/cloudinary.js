8110a05450dd057c479d658ea05cb4feb2695ee8
vikasutf8 <21bcs241@gamil.com>
2024-02-14 21:40:42 +0530

    multer middlewares and cloudinary utils

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,35 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+ //unlinking
+
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" }, 
+//   function(error, result) {console.log(result); });
+
+
+const uploadOnCloudinary  =async(localFilePath)=>{
+    try {
+        if(!localFilePath){
+            console.log( `Could not find path :`,localFilePath);
+            return NULL
+        }
+        // uploading
+        const response =await cloudinary.uploader.upload(localFilePath,{
+            resource_type:'auto'
+        })
+        console.log("uploaded file Successfully:" ,response.url)
+        return response;
+    } catch (err) {
+        // remove from my/local server filed
+        fs.unlinkSync(localFilePath);
+        console.log("File not upload and removed from local server")
+        return NULL;
+    }
+}
